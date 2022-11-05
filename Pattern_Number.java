d74f4fd2738cdd31e7c4f89bc33cc4d53a02c204
KavyaLakshmi98 <prasannapetam@gmail.com>
2022-11-05 15:18:43 +0530

    Create Pattern_Number.java
    Initial commit to pattern

---
+++ Pattern_Number.java
@@ -0,0 +1,23 @@
+package Interview;
+
+public class Pattern_Number {
+
+	public static void main(String[] args) {
+       int i,r = 6,j = 0;
+		for(i=1;i<=r;i++)
+		{
+			for(j=1;j<i;j++)
+			{
+				System.out.println();
+				
+		        for(j=1;j<=r;j++)
+			     {
+		        	System.out.print(j+" ");
+			   }
+			}
+		}
+		
+	}
+}
+       
+
