b46c9ce184c2b4e1050129ec4aa3178bd3cfa1f8
KavyaLakshmi98 <prasannapetam@gmail.com>
2022-11-05 15:56:20 +0530

    Create Positive_Negative.java
    commit

---
+++ Positive_Negative.java
@@ -0,0 +1,18 @@
+package Interview;
+
+public class Positive_Negative {
+
+	public static void main(String[] args) {
+		
+		int i=-5;
+		if(i>1)
+		{
+			System.out.println("postive");
+		}
+		else
+		{
+			System.out.println("negative");
+		}
+	}
+
+}
